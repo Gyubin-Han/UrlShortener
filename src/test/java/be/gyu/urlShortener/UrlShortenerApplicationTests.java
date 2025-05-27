bbb41725a84a9e066eeb433be9e55e56fe2ed7da
Gyubin-Han <yangglei2@naver.com>
2025-05-27 13:09:52 +0900

    First Commit

---
+++ src/test/java/be/gyu/urlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gyu.urlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
