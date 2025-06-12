182d065b37c2235fea76460394396bb63bced0ff
Gyubin-Han <yangglei2@naver.com>
2025-06-12 18:43:14 +0900

    Chore: DB - 각 ID 컬럼의 데이터 타입 및 Entity 수정

--- src/main/java/be/gyu/urlShortener/entity/UrlMap.java
+++ src/main/java/be/gyu/urlShortener/entity/UrlMap.java
@@ -2,28 +2,32 @@ package be.gyu.urlShortener.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Builder(toBuilder=true)
 public class UrlMap {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int urlMapId;
+    private Long urlMapId;
     private String urlMapOriginal;
     private String urlMapShort;
     private boolean urlMapIsActive;
     @ManyToOne
     @JoinColumn(name="user_id")
     private Users user;
-    private int urlMapClick;
+    @Column(insertable=false)
+    private Long urlMapClick;
     private LocalDateTime urlMapCreatedAt;
-    private LocalDateTime urlMapEditedAt;
+    private LocalDateTime urlMapUpdatedAt;
     private LocalDateTime urlMapExpiredAt;
 }
--- src/main/java/be/gyu/urlShortener/entity/Users.java
+++ src/main/java/be/gyu/urlShortener/entity/Users.java
@@ -6,14 +6,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Builder
 public class Users {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int userId;
+    private Long userId;
     private String userEmail;
     private String userPassword;
     private String userNickname;
