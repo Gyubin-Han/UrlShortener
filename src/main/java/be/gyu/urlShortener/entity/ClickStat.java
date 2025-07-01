e0fec299f96fc4c6ffef2d508e9731b4d007bafc
Gyubin-Han <yangglei2@naver.com>
2025-07-01 16:58:24 +0900

    Fix: 단축 URL 접속 정보 Entity의 ID 값 누락 수정

--- src/main/java/be/gyu/urlShortener/entity/ClickStat.java
+++ src/main/java/be/gyu/urlShortener/entity/ClickStat.java
@@ -3,6 +3,8 @@ package be.gyu.urlShortener.entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ClickStat {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long clickStatId;
     @ManyToOne
     @JoinColumn(name="url_map_id")
